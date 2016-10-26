677a41b00d8f2d186e3ba048e76faf50339e42bf
gurushant.jidagi <gurushant.jidagi@afourtech.com>
2016-10-26 17:13:40 +0530

    sample example of angular with repeat

--- angular_controller/login_controller.js
+++ angular_controller/login_controller.js
@@ -9,45 +9,22 @@ var loginDiv=angular.module('login',[]);
 
 loginDiv.controller('login_controller',function ($scope,$attrs,$http,$window){
 				$scope.login=function(){
-					// var config = {headers:  {
-    	//     					'Accept': 'application/json',
-     //    						'Content-Type':'application/json',
-    	// 									}
-  			// 					 };
 
   				console.log($scope.password);
   				console.log($scope.user_id);
-  				$window.location.href = 'item_list.html';
-  				
-					// $http.post("http://54.201.193.216:9090/rest/login",config).success(function(data)
-					// {
-					// 	console.log(data)
-					// }).
-					// error(function(status)
-					// {
-						
-					// });
-				 	// 			if($scope.user_id=='admin' &&  $scope.password=='admin')
-  				// {
-  				// 	$window.location.href='item_list.html';
-  				// }
-  				// else
-  				// {
-  				// 	alert("Invalid user id or password");
-  				// }
-				// isValid=verifyInput();
-				// if(isValid==true)
-				// {
-				// 	$http.post("http://54.218.148.213:9090/rest/login",JSON.stringify(delayArr, null, 4),config).success(function(data)
-				// 	{
-				// 		console.log(data)
-				// 	}).
-				// 	error(function(status)
-				// 	{
-						
-				// 	});
-			//}
-		 }	
+  				var isValid=verifyInput();
+  				if(isValid==true)
+  				{
+					$http.post("http://54.201.193.216:9090/rest/login").success(function(data)
+					{
+						$window.location.href = data;		
+					}).
+					error(function(status)
+					{
+						console.log("Error occured "+status);
+					});
+				}
+			 }	
 
 		function verifyInput()
 		  {
